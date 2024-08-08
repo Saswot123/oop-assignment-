f215e53822636837e9d87316357291b49a7c16fe
Saswot123 <128954020+Saswot123@users.noreply.github.com>
2024-08-09 00:25:43 +0545

    Update Driver.java

--- WeeklyExercises/7Inheritance/Driver.java
+++ WeeklyExercises/7Inheritance/Driver.java
@@ -2,8 +2,8 @@ package week6;
 
 public class Driver {
     public static void main(String[] args) {
-        FootballStats footballPlayer = new FootballStats("kaushal", 24, 10, 5, 2, 1);
-        CricketStats cricketPlayer = new CricketStats("umanga", 15, 50, 5);
+        FootballStats footballPlayer = new FootballStats("saswot", 24, 10, 5, 2, 1);
+        CricketStats cricketPlayer = new CricketStats("prasen", 15, 50, 5);
 
         System.out.println("Football Player Stats:");
         footballPlayer.displayStats();
